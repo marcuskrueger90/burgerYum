de24b2051061ff32c892eee2d6a2d86636bdedb6
Marcus Krueger <marcuskrueger90@gmail.com>
2020-01-25 12:30:29 -0500

    set up jawsdb table

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
